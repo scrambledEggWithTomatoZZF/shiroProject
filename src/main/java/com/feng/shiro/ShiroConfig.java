37e9ed48345a891ffa0bec49c9b362630dd62981
zhangzhifeng7 <zzhif@cndatacom.com>
2021-02-02 14:54:57 +0800

    公司 145653 新增thymeleaf模板引擎

---
+++ src/main/java/com/feng/shiro/ShiroConfig.java
@@ -0,0 +1,39 @@
+package com.feng.shiro;
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * author: zzf
+ * date: 2021年2月2日
+ */
+@Configuration
+public class ShiroConfig {
+
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager")DefaultWebSecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        //设置安全管理器
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        return shiroFilterFactoryBean;
+    }
+    /**
+     * 创建DefaultWebSecurityManager
+     */
+    @Bean(name="securityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("userRealm")UserRealm userRealm) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        //关联realm
+        securityManager.setRealm(userRealm);
+        return securityManager;
+    }
+    /**
+     * 创建Realm
+     */
+    @Bean(name="userRealm")
+    public UserRealm getRealm(){
+        return new UserRealm();
+    }
+}
---
+++ src/main/java/com/feng/shiro/UserRealm.java
@@ -0,0 +1,26 @@
+package com.feng.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * author: zzf
+ * date: 2021年2月2日
+ */
+public class UserRealm extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        System.out.println("执行授权逻辑");
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        System.out.println("执行认证逻辑");
+        return null;
+    }
+}
